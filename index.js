8235d1fe351a79f6d6f4c4e1fec969e29c1d5743
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-03 18:10:12 +0600

    Read a specific service with a new api in server, from database and send response to the client.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -17,11 +17,29 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    const serviceCollection = client.db("geniusCar").collection("services");
+    // R: read starT
+    // Create an API  in the server with get to  Read data in server site,  from database, and send response to the client
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    ///Read a specific service with a new api in server, from database and send response to the client
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    // R: read enD
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 //------------------
 
 app.get("/", (req, res) => {
