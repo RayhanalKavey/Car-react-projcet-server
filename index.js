3ca4344e16189ee5cd5d845557e649c80bf67fda
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-04 19:43:48 +0600

    create order api from update status, delete and read.

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("geniusCar").collection("services");
-    // R: read starT
+    const orderCollection = client.db("geniusCar").collection("orders");
+
+    // service api starT
     // Create an API  in the server with get to  Read data in server site,  from database, and send response to the client
     app.get("/services", async (req, res) => {
       const query = {};
@@ -35,7 +37,48 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
-    // R: read enD
+    // service api enD
+
+    //Orders api
+    //notE query operator to search order under a gmail
+    app.get("/orders", async (req, res) => {
+      // console.log(req.query.email);
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+    // orders information will come from the user form
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    //update user
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+    //Delete orders
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
