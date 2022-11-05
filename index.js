052ab5dc25e7659069d1938eedacf6cb768584f0
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-05 17:00:36 +0600

    Implement jwt token.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+// Jwt
+const jwt = require("jsonwebtoken");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -22,6 +25,17 @@ async function run() {
     const serviceCollection = client.db("geniusCar").collection("services");
     const orderCollection = client.db("geniusCar").collection("orders");
 
+    //workinG -------------JWT starT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      //create token
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    //JWT ----------------- enD
+
     // service api starT
     // Create an API  in the server with get to  Read data in server site,  from database, and send response to the client
     app.get("/services", async (req, res) => {
@@ -39,9 +53,44 @@ async function run() {
     });
     // service api enD
 
+    //workinG-------------------------------- JWT verify function starT
+    function verifyJWT(req, res, next) {
+      const authHeader = req.headers.authorization;
+      ///get token
+      if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access." });
+      }
+      //after getting code in the header get only the token
+      const token = authHeader.split(" ")[1];
+      // check validity of the token. Verify hole akta decoded dibe (decrepit here)
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+          if (err) {
+            return res.status(403).send({ message: "Forbidden access." });
+          }
+          req.decoded = decoded; ///set decoded value in the req
+          next();
+        }
+      );
+    }
+    //------------------------------------------JWT verify function enD
+
     //Orders api
-    //notE query operator to search order under a gmail
-    app.get("/orders", async (req, res) => {
+
+    //query operator to search order under a gmail
+    app.get("/orders", verifyJWT, async (req, res) => {
+      //---------------- workinG JWT  starT
+      // console.log(req.headers.authorization); // we see the authorization code here send from client site
+      const decoded = req.decoded; ///get decoded value from the req
+      // console.log("Inside orders API", decoded);
+      ////notE we provide token and verify the token if valid now we will decide we get the proper information related to this token
+      if (decoded.email !== req.query.email) {
+        res.status(401).send({ message: "Unauthorized access." }); //akhn kau http://localhost:5005/orders?email=rkavey5@gmail.com likhe data nite chaile ei error message ta dekhbe
+      }
+      //---------------- JWT  enD
+
       // console.log(req.query.email);
       let query = {};
       if (req.query.email) {
@@ -53,14 +102,14 @@ async function run() {
       const orders = await cursor.toArray();
       res.send(orders);
     });
-    // orders information will come from the user form
-    app.post("/orders", async (req, res) => {
+    // orders information will come from the user form //create
+    app.post("/orders", verifyJWT, async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
     //update user
-    app.patch("/orders/:id", async (req, res) => {
+    app.patch("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const status = req.body.status;
       const query = { _id: ObjectId(id) };
@@ -73,7 +122,7 @@ async function run() {
       res.send(result);
     });
     //Delete orders
-    app.delete("/orders/:id", async (req, res) => {
+    app.delete("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
